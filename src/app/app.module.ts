7cbb74e18d6ef094313a707a76f13b65549ac3e7
Anmol Vashistha <anmolvashisthaav@gmail.com>
2017-08-05 10:21:32 +0530

    Header & Footer
    search added to header
    new footer added and configured

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,11 @@ import { SharedModule } from './shared/shared.module';
 import { AuthModule } from './auth/auth.module';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -13,6 +13,7 @@ import { Router } from '@angular/router';
 
  export class HeaderComponent {
    @ViewChild('nav') el:ElementRef;
+   search=false;
    toggle=false;
 	 recipes=[];
 
